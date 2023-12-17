8ce4064abd0855f035dc56be5b671214f6cc91dc
M Faizal Akbar <faizal.akbar@nuon.id>
2023-12-17 20:27:46 +0700

    declare type

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 type User struct {
+	Id       int
+	Username string
+	Password string
+	Name     string
 }
 
-type Post struct {
+type Posts struct {
+	Id     int
+	UserId int
+	Post   string
 }
 
 type PostComment struct {
+	PostId   int
+	FriendId int
+	Comment  string
 }
 
 type Friends struct {
+	UserId   int
+	FriendId int
 }
 
 func main() {
