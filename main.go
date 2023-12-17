e5e5cff2f1195cb9ecda83a7881bddf0b7e59296
M Faizal Akbar <faizal.akbar@nuon.id>
2023-12-17 20:14:54 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+type User struct {
+}
+
+type Post struct {
+}
+
+type PostComment struct {
+}
+
+type Friends struct {
+}
+
+func main() {
+
+}
+
+func userRegister() {}
+func userLogin()    {}
+func editProfile()  {}
+func addFriend()    {}
+func removeFriend() {}
+func friendList()   {}
+func searchUser()   {}
